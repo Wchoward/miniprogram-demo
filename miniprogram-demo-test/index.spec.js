02b63f785840d3907a295c3db123a5681f6db1e7
Howard WU <Wchoward@users.noreply.github.com>
2020-04-21 15:35:11 +0800

    test

--- miniprogram-demo-test/index.spec.js
+++ miniprogram-demo-test/index.spec.js
@@ -3,6 +3,7 @@ const automator = require('miniprogram-automator')
 describe('index', () => {
   let miniProgram
   let page
+  let test
   beforeAll(async () => {
     miniProgram = await automator.launch({
       projectPath: './'
